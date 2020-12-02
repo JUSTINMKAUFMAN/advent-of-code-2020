06f3a41c5b9f4120c2f08e9f07147eaef6afc9bd
Justin Kaufman <justinkaufman@98point6.com>
2020-12-01 22:36:11 -0800

    oops

--- day2/day2.swift
+++ day2/day2.swift
@@ -1,7 +1,18 @@
-struct Rule {
-    let char: String
+struct ValidationRule {
+    let char: Character
     let min: Int
     let max: Int
+
+    func isValidA(_ password: String) -> Bool {
+        let count = password.filter { $0 == char }.count
+        return count >= min && count <= max
+    }
+    
+    func isValidB(_ password: String) -> Bool {
+        let a = password[safe: min - 1] == char
+        let b = password[safe: max - 1] == char
+        return (a && !b) || (!a && b)
+    }
 }
 
 let array: [(ValidationRule, String)] = text.components(separatedBy: "\n").map { entry in
